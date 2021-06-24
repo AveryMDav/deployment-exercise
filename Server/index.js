d57828138d2a4cac44f6890eecff5bd50db3cd5e
Avery.Davenport <averymdavenport@gmail.com>
2021-06-24 13:45:00 -0600

    3

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 
 const path = require('path');
@@ -6,11 +7,11 @@ const path = require('path');
 app.get('/', function(req, res){
     
     res.sendFile(path.join(__dirname,'../index.html'));
+
     
 })
 
-
-
-
 const port = process.env.PORT || 5050
-app.listen(port, function(){console.log(`Server is gaining sentience on ${port}`)});
+
+app.listen(port, function(){
+    console.log(`Server is gaining sentience on ${port}`)});
